4b2f8d3ff976abdaf43a22c408bbc00ee701ae97
cosmanaute <cosmanaute@gmail.com>
2023-06-29 18:27:34 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use webbrowser;
 use std::env;
+use std::io;
+use webbrowser;
 
 fn web_transform(url: &Vec<String>) -> String {
     let mut transformed = String::new();
@@ -7,28 +8,26 @@ fn web_transform(url: &Vec<String>) -> String {
         transformed.push_str(&element.to_string());
         transformed.push('+');
     }
-    return transformed
+    return transformed;
 }
 
-fn main() {
-
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let mut url = String::new();
-    if &args[1] == "-g" {
-        url.push_str("https://www.google.com/search?q=");
-        url.push_str(&web_transform(&args));
-        webbrowser::open(&url).expect("failed to open URL");
-    }
-    else if &args[1] == "-y" {
-        url.push_str("https://www.youtube.com/results?search_query=");
-        url.push_str(&web_transform(&args));
-        webbrowser::open(&url).expect("failed to open URL");
-    }
-    else {
-        webbrowser::open(&args[1]).expect("failed to open URL");
-    }
 
+    match args[1].as_str() {
+        "-g" => {
+            url.push_str("https://www.google.com/search?q=");
+            url.push_str(&web_transform(&args));
+            webbrowser::open(&url)?;
+        }
+        "-y" => {
+            url.push_str("https://www.google.com/search?q=");
+            url.push_str(&web_transform(&args));
+            webbrowser::open(&url).expect("failed to open URL");
+        }
+        _ => webbrowser::open(&args[1])?,
+    };
 
-
-
+    Ok(())
 }
