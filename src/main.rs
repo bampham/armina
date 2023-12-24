0c00af4009891880c7041815413aa2cfb6093d63
Cosmanaute <cosmanaute@gmail.com>
2023-12-24 14:20:45 +0100

    added error msg and flag for twitch

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::env;
 use std::io;
 use webbrowser;
 
+const HELP: &str = "[-g google <search promt>] [-y youtube <search promt>] 
+[-w browser <website>] [-t twitch <channel>]";
+
 fn web_transform(url: &Vec<String>) -> String {
     let mut transformed = String::new();
     for element in url.iter().skip(2) {
@@ -13,8 +16,12 @@ fn web_transform(url: &Vec<String>) -> String {
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let mut url = String::new();
+    if args.len() == 1 {
+        println!("{HELP}");
+        std::process::exit(1);
+    }
 
+    let mut url = String::new();
     match args[1].as_str() {
         "-g" => {
             url.push_str("https://www.google.com/search?q=");
@@ -24,12 +31,17 @@ fn main() -> io::Result<()> {
         "-y" => {
             url.push_str("https://www.youtube.com/results?search_query=");
             url.push_str(&web_transform(&args));
-            webbrowser::open(&url).expect("failed to open URL");
+            webbrowser::open(&url)?;
         }
         "-w" => {
             url.push_str("https://www.");
             url.push_str(&args[2]);
-            webbrowser::open(&url).expect("failed to open URL");
+            webbrowser::open(&url)?;
+        }
+        "-t" => {
+            url.push_str("https://www.twitch.tv/");
+            url.push_str(&args[2]);
+            webbrowser::open(&url)?;
         }
         _ => webbrowser::open(&args[1])?,
     };
